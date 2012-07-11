1569e6733847caf37ea3fc5c6fa87928c0236c2c
Zero <john.royer@gmail.com>
2012-07-11 17:26:54 +0800

    放置樣板
    所有樣板 (template) 均放置於 application/views/ 目錄下，記得將
    樣板副檔名改為 php，CodeIgniter 才抓的到。

---
+++ application/views/book.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+   <title>Book</title>
+   
+</head>
+
+<body>
+   <form action="#" method="post" accept-charset="utf-8">
+      <p><label for="ID:">ID:</label><input type="text" name="id" value="" id="id"></P>
+      <p><textarea name="comment" rows="8" cols="40"></textarea></p>
+      <p><input type="submit" name="some_name" value="留言"></p>
+   </form>
+
+   <hr>
+
+   <p>somebody said:  blablabla ......</p>
+
+   <hr>
+</body>
+</html>
