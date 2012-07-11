29b8aab8d070fa48cbb97dc9080209b3defb1392
Zero <john.royer@gmail.com>
2012-07-11 18:22:01 +0800

    連接資料庫
    Controller 與 view 已經建立完成，現在連接資料庫準備將留言板的資料
    撈出來。
    
    資料庫設定檔位於 application/config/database.php，將資料庫連接所需
    要的資訊填入，CodeIgniter 便會使用這些設定與資料庫建立連線。
    
    並不是所有的資料都必須填寫，例如 dbdriver 為 sqlite 時，username、
    password 可以留空白。

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'cidemo';
+$db['default']['password'] = '123';
+$db['default']['database'] = 'ci-guestbook';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
